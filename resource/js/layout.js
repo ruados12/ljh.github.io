15b6ca43f8fe3842949e31fabe897de8efdd0117
ruados12 <ruados12@gmail.com>
2024-10-02 00:21:57 +0900

    t

---
+++ resource/js/layout.js
@@ -0,0 +1,277 @@
+// const rand = (min, max) => {
+//     ~~ 연산자는 소수점 이하를 버리고 정수로 반환
+//     return ~~(Math.random() * (max - min + 1)) + min;
+// }
+
+let w, h;
+w = window.innerWidth;
+h = window.innerHeight;
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    lerp: 0.075,
+
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+    scrollTop(value) {
+        return arguments.length
+            ? locoScroll.scrollTo(value, { duration: 0, disableLerp: true })
+            : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+        return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+        };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("[data-scroll-container]").style.transform
+        ? "transform"
+        : "fixed",
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: "[data-scroll-container]" });
+
+
+AOS.init();
+
+
+class Layout {
+    constructor() {
+        this.header = document.querySelector("#header");
+        this.toggle = this.header.querySelector(".toggle_btn");
+        this.allMenu = document.querySelector("#allMenu");
+
+        this.scrollHeaderEvt();
+    }
+    toggleEvt() {
+        this.toggle.addEventListener("click", (evt) => {
+            this.toggle.classList.toggle("on");
+            if (this.toggle.classList.contains("on")) {
+                this.openAllMenu();
+            } else {
+                this.closeAllMenu();
+            }
+        });
+    }
+    scrollHeaderEvt() {
+        const scrollEvt = () => {
+            if (window.scrollY >= 80) {
+                this.header.classList.add("hide");
+            } else {
+                this.header.classList.remove("hide");
+            }
+        };
+        window.addEventListener(
+            "scroll",
+            throttle(() => scrollEvt(), 100)
+        );
+    }
+    openAllMenu() {
+        this.allMenu.classList.add("on");
+        bubble.activeBubble();
+    }
+    closeAllMenu() {
+        this.allMenu.classList.remove("on");
+        this.toggle.classList.remove("on");
+        bubble.cancelBubble();
+    }
+}
+
+class Bubble {
+    constructor() {
+        this.cvs = document.querySelector("#bubble");
+        this.ctx = this.cvs.getContext("2d");
+        this.bubble = [];
+        this.square = {};
+        this.w = window.innerWidth;
+        this.h = window.innerHeight;
+        this.square = {
+            w: this.w,
+            h: this.h,
+            y: this.h,
+            speed: 0.25,
+            totalTime: 250,
+            duration: 250,
+            time: 0,
+            delay: 15,
+        };
+        this.cvs.width = this.w;
+        this.cvs.height = this.h;
+        this.bubbleCount = 200;
+        // this.color = `rgba(255,176,158,.75)`;
+        this.color = `rgba(255,176,158,1)`;
+        this.anim;
+        this.ctx.clearRect(0, 0, this.w, this.h);
+        for (let i = 0; i < this.bubbleCount; i++) {
+            this.bubble.push(this.getBubble());
+        }
+    }
+    activeBubble() {
+        if (this.anim) cancelAnimationFrame(this.anim);
+        const render = () => {
+            this.ctx.clearRect(
+                this.w * -0.5,
+                this.h * -0.5,
+                this.w * 1.5,
+                this.h * 1.5
+            );
+            this.ctx.fillStyle = this.color;
+            if (Number(this.square.y) > 0) {
+                // 가속도
+                let sq = this.square;
+                sq.totalTime - +sq.time;
+                // ease-out
+                sq.y =
+                    sq.y +
+                    ((sq.time - sq.delay) / sq.duration) *
+                        sq.speed *
+                        (0 - sq.y);
+                this.square.time += 1;
+            } else {
+                this.square.y = 0;
+                this.totalTime = this.duration;
+                cancelAnimationFrame(this.anim);
+            }
+            this.ctx.beginPath();
+            this.ctx.rect(0, this.square.y, this.square.w, this.square.h);
+            this.ctx.fill();
+            for (const bb of this.bubble) {
+                this.ctx.beginPath();
+                this.ctx.arc(bb.x, bb.y, bb.r, 0, Math.PI * 2);
+                this.ctx.fillStyle = this.color;
+                this.ctx.fill();
+
+                bb.y = bb.y - bb.speed * 1;
+                if (bb.y <= bb.r * -2) {
+                    bb.y = rand(this.h, this.h * 2);
+                    bb.speed = rand(1, 25);
+                }
+            }
+            this.anim = requestAnimationFrame(render);
+        };
+        this.anim = requestAnimationFrame(render);
+    }
+    cancelBubble() {
+        if (this.anim) {
+            cancelAnimationFrame(this.anim);
+            this.ctx.clearRect(0, 0, this.w, this.h);
+            this.bubble = [];
+            this.square.y = this.h;
+            this.square.time = 0;
+            this.totalTime = this.duration;
+            for (let i = 0; i < this.bubbleCount; i++) {
+                this.bubble.push(this.getBubble());
+            }
+        }
+    }
+
+    getBubble() {
+        const b = {};
+        b.x = rand(0, this.w);
+        b.y = rand(this.h, this.h * 2.5);
+        b.r = rand(50, 175);
+        b.speed = rand(5, 35);
+        // b.speed = 100;
+        return b;
+    }
+}
+
+class Loading {
+    constructor() {
+        this.w = window.innerWidth;
+        this.cvs = document.createElement("canvas");
+        this.ctx = this.cvs.getContext("2d");
+        this.cvs.width = wid;
+        this.cvs.height = hei;
+        this.bgColor = "#002025 ";
+        this.ctx.fillRect(0, 0, w, h);
+        this.ctx.fill();
+
+        this.cvs.classList.add("loading_wrap");
+        this.cvs.id = "loadingWrap";
+
+        this.centerLine = hei / 2;
+        this.amplitude = hei / 5; // 높이
+        this.frequency = 0.035; // 폭
+        this.gap = 20; // 간격
+        this.velocity = 0.025; // 속도
+
+        this.lines = [];
+        for (let s = 0; s < 25; s++) {
+            this.points = [];
+            for (let i = 0; i <= Math.ceil(this.cvs.width / this.gap); i++) {
+                const x = i * this.gap;
+                let y;
+                this.points.push(this.createPoint(x, y, i));
+            }
+            this.lines.push(this.points);
+        }
+    }
+    init() {
+        document.body.append(this.cvs);
+        this.render();
+    }
+    clear() {
+        this.ctx.clearRect(0, 0, cvs.width, cvs.height);
+    }
+    remove() {
+        if (document.querySelector("#loadingWrap")) {
+            this.cvs.remove();
+        }
+    }
+    createPoint(x, y, i) {
+        return { x: x, y: y, radian: i * this.frequency, i: i };
+    }
+    render() {
+        this.ctx.fillStyle = this.bgColor;
+        this.ctx.strokeStyle = "rgba(255,255,255,0.85)";
+        this.ctx.lineWidth = 0.125;
+        let tempTime = 0;
+        let delay = this.lines.length;
+        let animation = (timeStamp) => {
+            let time = Math.ceil(timeStamp * 0.002);
+
+            this.ctx.clearRect(0, 0, cvs.width, cvs.height);
+            this.ctx.fillRect(0, 0, w, h);
+            this.ctx.fill();
+            for (let s = 0; s < Object.keys(this.lines).length; s++) {
+                this.ctx.beginPath();
+                let points = this.lines[s];
+                for (let i = 0; i < points.length; i++) {
+                    let p = points[i];
+                    p.radian += this.velocity;
+                    p.y =
+                        this.amplitude * Math.sin(p.radian + s * 0.05) +
+                        this.centerLine;
+                    this.ctx.lineTo(p.x, p.y);
+                }
+                this.ctx.stroke();
+            }
+            this.anim = requestAnimationFrame(animation);
+        };
+        this.anim = requestAnimationFrame(animation);
+    }
+    resizeLoading() {
+        this.cvs.width = wid;
+        this.cvs.height = hei;
+    }
+}
+
+const layout = new Layout();
+const bubble = new Bubble();
+layout.toggleEvt();
+
+const loading = new Loading();
+// loading.init();
---
+++ resource/js/sub.js
@@ -0,0 +1,450 @@
+class Paging {
+    constructor() {
+        this.subWrap = document.querySelector('.sub_wrap');
+        this.subBox = this.subWrap.querySelector('.sub_box');
+        this.colors = ['#2a2a2a','#3a3a3a'];
+
+        this.btns = Array.from(document.querySelectorAll('.dep_tit'));
+        this.btns.forEach(el => {
+            el.addEventListener('click', evt => this.movePage(evt));
+        });
+    }
+   
+    movePage(evt) {
+        const tg = evt.currentTarget;
+        const pageName = this.selectPage(tg.name);
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open("GET", `/${pageName}.html`, true);
+        this.xhr.send();
+        this.xhr.onload = () => {
+            stopParticle();
+            if(pageName === 'index') {
+                startParticle();
+            }else {
+                this.subWrap.classList.add('on');
+                switchPage.actionAnim()
+            }
+            if(this.xhr.status == 200) {
+                const html = this.xhr.responseText;
+                this.subBox.innerHTML = html;
+                if(pageName == 'works') {
+                    platte = new Platte();
+                }
+            }else {
+                // 실패
+                console.log("HTTP 응답 오류: " + xhr.status);
+            }
+        }
+    }
+    selectPage(name) {
+        switch(name) {
+            case 'about':
+                stopParticle();
+                this.color = this.colors[0];
+                return 'about';
+                break;
+            case 'works':
+                this.color = this.colors[1];
+                stopParticle();
+                return 'works';
+                break;
+            default:
+                this.color = '';
+                return 'index';
+                break;
+        }
+    }
+}
+class SwitchPage {
+    constructor() {
+        this.subWrap = document.querySelector('.sub_wrap');
+        this.subBox = this.subWrap.querySelector('.sub_box');
+        this.cvs = this.subWrap.querySelector('#subCvs');
+        this.ctx = this.cvs.getContext('2d');
+        
+        this.cvs.width = wid;
+        this.cvs.height = hei;
+
+
+        this.ctx.clearRect(0, 0, wid, hei);
+
+        this.render;
+
+        this.createArc();
+
+        // particles;
+        this.particleCount = 125;
+    };
+    createParticle() {
+        let p = {};
+        p.x = this.cvs.width / 2,
+        p.y = this.arc.y;
+        p.r = rand(4, 12);
+        p.endX = p.x - rand(-150, 150);
+        p.endY = p.y - rand(-150, 150);
+        p.alpha = 1;
+        p.duration = randFloat(0.5, 2.5);
+        return p;
+    };
+    drawParticle(p, idx) {
+        gsap.to(p, {
+            duration: p.duration,
+            x: p.endX,
+            y: p.endY,
+            alpha: 0,
+            ease: "expoScale(0.5,7,none)",
+            r: 1,
+            onComplete: () => {
+                if(this.isParticle) {
+                    let p = this.createParticle();
+                    this.particles.splice(idx, 1, p);
+                    this.drawParticle(p, idx);
+                }
+            }
+        });
+    }
+    actionAnim() {
+        this.particles = [];
+        this.createArc();
+        for(let i = 0; i < this.particleCount; i++) {
+            let p = this.createParticle();
+            this.particles.push(p);
+            this.drawParticle(p, i);
+        }
+        
+        this.dropArcAnim(this.arc);
+
+        this.isParticle = true;
+        this.cvs.classList.remove('hide');
+        this.anim = requestAnimationFrame(this.drawDropAnim.bind(this));
+    }
+    drawDropAnim() {
+        this.ctx.beginPath();
+        this.ctx.clearRect(0, 0, wid, hei);
+        this.ctx.arc(this.arc.x, this.arc.y, this.arc.r, 0, Math.PI * 2);
+        var grd = this.ctx.createRadialGradient(this.arc.x, this.arc.y, 15, this.arc.x, this.arc.y, 100);
+        grd.addColorStop(0, "rgba(255,255,255, 1)");
+        grd.addColorStop(0.25, "rgba(255, 238, 195, 0.25)");
+        grd.addColorStop(1, "transparent");
+        this.ctx.fillStyle = grd;
+        this.ctx.fill();
+
+        for(let p of this.particles) {
+            this.ctx.beginPath();
+            this.ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
+            this.ctx.fillStyle = `rgba(255,255,255,${p.alpha})`;
+            this.ctx.fill();
+        }
+        this.anim = requestAnimationFrame(this.drawDropAnim.bind(this));
+    };
+    drawScaleAnim() {
+        this.ctx.beginPath();
+        this.ctx.clearRect(0, 0, wid, hei);
+        this.ctx.arc(this.arc.x, this.arc.y, this.arc.r, 0, Math.PI * 2);
+        var grd = this.ctx.createRadialGradient(this.arc.x, this.arc.y, Math.floor(this.arc.r / 6.6666), this.arc.x, this.arc.y, this.arc.r);
+        grd.addColorStop(0, "rgba(40,42,58, 1)");
+        grd.addColorStop(0.25, "#3b3d4d");
+        grd.addColorStop(1, "#3b3d4d");
+        this.ctx.fillStyle = grd;
+        // this.ctx.fillStyle = "rgba(0,0,0,1)";
+        this.ctx.fill();
+
+        this.anim = requestAnimationFrame(this.drawScaleAnim.bind(this));
+    }
+    dropArcAnim(arc) {
+        this.dropTimeline = gsap.timeline()
+        this.dropTimeline.to(arc, {
+            delay: .25,
+            duration: 2,
+            y: this.center.y,
+            ease: "power1.out",
+            onComplete: () => {
+                this.isParticle = false;
+            },
+        });
+        this.dropTimeline.to(arc, {
+            delay: .25,
+            duration: 1,
+            r: wid,
+            onStart: () => {
+                cancelAnimationFrame(this.anim);
+                this.anim = requestAnimationFrame(this.drawScaleAnim.bind(this));
+            },
+            onComplete: () => {
+                layout.closeAllMenu();
+                cancelAnimationFrame(this.anim);
+                this.cvs.classList.add('hide');
+            }
+        })
+    };
+    createArc() {
+        this.center = {
+            x:  this.cvs.width / 2,
+            y:  this.cvs.height / 2
+        }
+        this.arc = {
+            x: this.cvs.width / 2,
+            r: 100,
+        }
+        this.arc.y = -this.arc.r;
+    }
+}
+
+class Platte {
+    constructor() {
+        this.works = document.querySelector(".work_lists");
+        this.lists = this.works.querySelector('.work_list');
+        this.lists.style.transform = `rotate(-90deg)`;
+        let rects = Array.from(this.works.querySelectorAll('.work_item'));
+        this.cnt = rects.length;
+        this.startX, this.startY, this.endX, this.endY;
+        let count = 0;
+        for (let rect of rects) {
+            rect.dataset.seq = count++;
+            if(this.cnt++ >= 20) break;
+            this.lists.appendChild(rect.cloneNode(true));
+        }
+        
+        this.rects = Array.from(this.works.querySelectorAll('.work_item'));
+
+        // const radius = hei / 2;
+        this.radius = wid / 1.75;
+        // this.radius = wid / 5;
+        this.angle = 360 / this.rects.length;
+        for (let i = 0; i < this.rects.length; i++) {
+            let rect = this.rects[i];
+            rect.style.transform = `rotate(${this.angle * i}deg) translate(${this.radius}px)`;
+            rect.addEventListener('dragstart', this.disabledDrag);
+            rect.addEventListener('click', this.changeClickPlatte.bind(this, event));
+        }
+        this.activeIdx = 0;
+        this.rects[this.activeIdx].classList.add('on');
+
+        scaleImg = new ScaleWorks();
+        scaleImg.changeScaleImg(this.activeidx)
+
+        this.works.addEventListener('dragstart', this.disabledDrag);
+        this.works.addEventListener('touchstart', (evt) => this.touchStart(this, evt), true);
+        this.works.addEventListener('mousedown', (evt) => this.touchStart(this, evt), true);
+        this.works.addEventListener('touchend', (evt) => this.touchEnd(this, evt), true);
+        this.works.addEventListener('mouseup', (evt) => this.touchEnd(this, evt), true);
+
+
+        // tooltip
+        this.tooltip = {};
+        this.tooltip.wrap = document.querySelector('#infoPop')
+        this.tooltip.tit = this.tooltip.wrap.querySelector('.popup_tit');
+        this.tooltip.url = this.tooltip.wrap.querySelector('.st_url');
+        this.tooltip.progress = this.tooltip.wrap.querySelector('.progress');
+        this.tooltip.conts = this.tooltip.wrap.querySelector('.txt_cont');
+        this.tooltip.closeBtn = this.tooltip.wrap.querySelector('.close_btn');
+        this.tooltip.closeBtn.addEventListener('click', () => {
+            this.tooltip.wrap.classList.remove('on');
+        })
+    }
+    changeClickPlatte() {
+        // 클릭해서 전환시
+        let selectIdx, step;
+        for(let i = 0; i < this.rects.length; i++) {
+            const rect = this.rects[i];
+            if(rect == event.currentTarget) {
+                selectIdx = i;
+            }
+        }
+        if(selectIdx == this.activeIdx) {
+            this.onTooltip();
+            return;
+        }
+        step = selectIdx - this.activeIdx;
+        if(this.activeIdx <= selectIdx && Math.abs(step) > 3 ) {
+            step = step - this.rects.length;
+        }else if(this.activeIdx >= selectIdx && Math.abs(step) > 3) {
+            step = step + this.rects.length;
+        }
+        
+        this.moveCycle(selectIdx, step)
+    }
+
+    onTooltip() {
+        event.preventDefault();
+        const tg = event.currentTarget;
+        if(!tg.classList.contains('on')) return false;
+        if(!tg.dataset.seq) return;
+        getRequest(tg.dataset.seq, '/detail.json').then((result) => {
+            this.openDetailPop(result);
+        });
+    };
+    openDetailPop(data) {
+        this.tooltip.tit.innerHTML = data.tit;
+        if(data.url != '' && data.url != '-') {
+            this.tooltip.url.innerHTML = data.url;
+            this.tooltip.url.href = data.url;
+        }else {
+            this.tooltip.url.innerHTML = '-';
+            this.tooltip.url.href = '#';
+        }
+        this.tooltip.progress.dataset.progress = data.progress;
+        this.tooltip.progress.style.setProperty('--progress',data.progress * 0.01);
+        this.tooltip.conts.innerHTML = data.contents;
+        this.tooltip.wrap.classList.add('on');
+    }
+
+    disabledDrag() {
+        return false;
+    }
+    changeActiveEl() {
+        this.rects.filter(el => {el.classList.remove('prev', 'next');});
+        if(this.activeIdx >= this.rects.length) this.activeIdx = 0;
+        if(this.activeIdx < 0) this.activeIdx = this.rects.length - 1;
+        this.rects[this.activeIdx].classList.add('on');
+        const prevElement = this.activeIdx <= 0 ? this.rects.length - 1 : Number(this.activeIdx) - 1;
+        const nextElement = this.activeIdx >= this.rects.length - 1 ? 0 : Number(this.activeIdx) + 1;
+        this.rects[prevElement].classList.add('prev');
+        this.rects[nextElement].classList.add('next');
+        scaleImg.changeScaleImg(this.activeidx)
+    }
+    moveCycle(act, step) {
+        this.lists.style.transform = 'rotate('+(parseInt(this.getDeg()) - Math.floor(this.angle) * step)+'deg)';
+        this.rects[this.activeIdx].classList.remove('on');
+        this.activeIdx = act;
+        this.changeActiveEl();
+    }
+    nextCycle() {
+        this.lists.style.transform = 'rotate('+(parseInt(this.getDeg()) - Math.floor(this.angle))+'deg)';
+        this.rects[this.activeIdx++].classList.remove('on');
+        this.changeActiveEl();
+    }
+    prevCycle() {
+        this.lists.style.transform = 'rotate('+(parseInt(this.getDeg()) + Math.floor(this.angle))+'deg)';
+        this.rects[this.activeIdx--].classList.remove('on');
+        this.changeActiveEl();
+    }
+    getDeg() {
+        var deg = this.lists.style.transform;
+        return deg.substring(deg.indexOf('(') + 1, deg.indexOf('deg'));
+    }
+
+    touchStart(t, e) {
+        t.start = {};
+        if(e.type == 'mousedown') {
+            t.start.x = parseInt(e.screenX);
+            t.start.y = parseInt(e.screenY);
+        }else {
+            t.start.x = parseInt(e.changedTouches[0].screenX);
+            t.start.y = parseInt(e.changedTouches[0].screenY);
+        }
+    }
+    touchEnd(t, e) {
+        t.end = {};
+        if(e.type == 'mouseup') {
+            t.end.x = parseInt(e.screenX)
+            t.end.y = parseInt(e.screenY)
+        }else {
+            t.end.x = parseInt(e.changedTouches[0].screenX);
+            t.end.y = parseInt(e.changedTouches[0].screenY);
+        }
+        t.dx = t.start.x - t.end.x;
+        t.dy = t.start.x - t.end.x;
+        // x, y축 이동이 100 이하면 단순 클릭으로 처리
+        if(!(Math.abs(t.dx) > 50 || Math.abs(t.dy) > 50)) return;
+        if(Math.abs(t.dx) > Math.abs(t.dy)) {
+            // X축 이동이 Y축 이동보다 클 경우
+            t.dx < 0 ? t.nextCycle() : t.prevCycle();
+        }else {
+            t.dy < 0 ? t.prevCycle() : t.nextCycle();
+        }
+    }
+}
+class ScaleWorks {
+    constructor() {
+        this.wrap = document.querySelector('#workWrap');
+        this.cvs = this.wrap.querySelector('#scaleCvs');
+        this.cvs.width = wid;
+        this.cvs.height = hei;
+        this.ctx = this.cvs.getContext('2d');
+
+        const arr = Array.from(this.wrap.querySelectorAll('.card_img img'));
+        const uniqueArr = arr.reduce((acc, el) => {
+            if (!acc.includes(el)) {
+              acc.push(el);
+            }
+            return acc;
+        }, []);
+        const tempCvs = document.createElement('canvas');
+        const tempCtx = tempCvs.getContext('2d');
+        tempCtx.clearRect(0, 0, wid, hei);
+        for(let el of uniqueArr) {
+            const img = new Image();
+            img.src = el.src;
+            img.onload = () => {
+                tempCtx.drawImage(img, 0, 0, img.width, img.height);
+            }
+        }
+
+        this.works = Array.from(this.wrap.querySelectorAll('.work_item'));
+    }
+    changeScaleImg(idx) {
+        const activeEl = this.works.filter(el => {return el.classList.contains('on')})[0];
+        let imgEl = activeEl.querySelector('img');
+        let path = this.getScalePath(imgEl);
+        const img = new Image();
+        img.onload = () => {
+            this.setScaleImg(img);
+        }
+        img.src = path;
+    }
+    getScaleImg(img) {
+        const imgWid = img.naturalWidth;
+        const imgHei = img.naturalHeight;
+
+        const imgRatio = imgWid / imgHei;
+        const docRatio = wid / hei;
+        let scaleHei, scaleWid, left, top;
+        if(imgRatio >= 1) {
+            // 이미지가 가로가 큰 경우
+            if(imgRatio >= docRatio) {
+                scaleHei = hei;
+                scaleWid = scaleHei * imgRatio;
+                left = (wid - scaleWid) / 2;
+                top = 0;
+            }else {
+                scaleWid = wid;
+                scaleHei = scaleWid / imgRatio;
+                left = 0;
+                top = (hei - scaleHei) / 2;
+            }
+        }else{
+            // 이미지가 세로가 큰 경우
+            if(imgRatio >= docRatio) {
+                scaleHei = hei;
+                scaleWid = scaleHei / imgRatio;
+                left = (wid - scaleWid) / 2;
+                top = 0;
+            }else {
+                scaleWid = wid;
+                scaleHei = scaleWid * imgRatio;
+                left = 0;
+                top = (hei - scaleHei) / 2;
+            }
+        }
+
+        return {
+            w: scaleWid,
+            h: scaleHei,
+            left: left,
+            top: top,
+        }
+    }
+    setScaleImg(img) {
+        const info = this.getScaleImg(img);
+        this.ctx.clearRect(0, 0, wid, hei);
+        this.ctx.drawImage(img, info.left, info.top, info.w, info.h);
+    }
+    getScalePath(el) {
+        return el.src.slice(0, el.src.lastIndexOf(".")) + "_scale" + el.src.slice(el.src.lastIndexOf("."));
+    }
+}
+// const page = new Paging();
+// const switchPage = new SwitchPage();
+let platte; // works
+let scaleImg;
